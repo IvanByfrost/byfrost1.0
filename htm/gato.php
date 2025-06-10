7e22ae0cd1ec0644a7511954b25bd80dacf209c5
IvanByFrost <ivanwander99@gmail.com>
2025-06-09 20:22:56 -0500

    Add HTML files for contact form, home page, login, registration, and terms of service; implement styles and structure for user interaction and data collection.

---
+++ htm/gato.php
@@ -0,0 +1,16 @@
+require_once 'config/app.php';
+
+include VIEWS_PATH . 'includes/head/head.php';
+include VIEWS_PATH . 'links.php';
+include VIEWS_PATH . 'includes/header/header.php';
+?>
+
+<body>
+
+
+
+
+    <?php
+    include 'app/views/includes/footer/footer.php';
+    include 'app/views/includes/home-scripts.php';
+    ?>
