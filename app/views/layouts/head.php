0f5f673247ba7053fcbc1d056778964b06da3663
IvanByfrost <ivanwander99@gmail.com>
2025-06-15 10:40:57 -0500

    Remove unused CSS links from head.php and correct plans link in header.php

--- app/views/layouts/head.php
+++ app/views/layouts/head.php
@@ -13,12 +13,10 @@ require_once ROOT . '/app/config.php';
     <title>Byfrost</title>
     <link rel="stylesheet" href="<?php echo url . rq ?>css\homeStyle.css">
     <link rel="stylesheet" href="<?php echo url . rq ?>css\loginstyle.css">
-    <!--<link rel="stylesheet" href="<?php echo url . rq ?>css\header.css">
-    <link rel="stylesheet" href="<?php echo url . rq ?>css\footer.css">
-    <link rel="stylesheet" href="<?php echo url . rq ?>css\home.css">-->
     <link rel="stylesheet" href="<?php echo url . rq ?>css\features.css">
     <link rel="stylesheet" href="<?php echo url . rq ?>css\slider.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0">
+    <link rel="icon" type="image/x-icon" href="<?php echo url . rq ?>img\favicon.png">
 </head>
