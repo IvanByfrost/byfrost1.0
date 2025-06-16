9fa6a1264109cea6f4f588eade3c8322d2bc6697
IvanByFrost <ivanwander99@gmail.com>
2025-06-16 05:34:52 -0500

    Fix .htaccess rewrite rule and improve error handling in ErrorController

--- app/controllers/errorController.php
+++ app/controllers/errorController.php
@@ -1,19 +1,22 @@
 <?php
 class ErrorController extends MainController
 {
-    public function Error($message = null)
+    public function error($message = null)
     {
         $errorCode = $_GET['error'] ?? null;
 
         switch ($errorCode) {
             case '400':
-                $this->view->Render('Error', '400');
+                $this->render('Error/400');
+                break;
+            case '404':
+                $this->render('Error/404');
                 break;
             case '500':
-                $this->view->Render('Error', '500');
+                $this->render('Error/500');
                 break;
             default:
-                $this->view->Render('Error', 'error', $message);
+                $this->render('Error/error', ['message' => $message]);
         }
     }
 }
