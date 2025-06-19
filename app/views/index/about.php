b29f125049ca81eb5a50d651d168b4bfa59d07ee
IvandProgram <ivandprogramador@gmail.com>
2025-06-19 15:06:28 -0500

    fix: add placeholder quotes for team members in about page

--- app/views/index/about.php
+++ app/views/index/about.php
@@ -14,21 +14,25 @@ require_once __DIR__ . '/../layouts/header.php';
             <img src="<?php echo url . rq ?>img\team1.webp" alt="Team Member 1">
             <h3>Iván Ruiz</h3>
             <p>CEO - Scrum Master</p>
+            <p>"Cita del día"</p>
         </div>
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team2.webp" alt="Team Member 1">
             <h3>Johan Carrillo</h3>
             <p>Backend Developer</p>
+            <p>"Cita del día"</p>
         </div>
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team3.webp" alt="Team Member 1">
             <h3>Lucía Nova</h3>
             <p>Backend Developer</p>
+            <p>"Cita del día"</p>
         </div>
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team4.webp" alt="Team Member 1">
             <h3>Laura Ariza</h3>
             <p>Frontend Developer</p>
+            <p>"Cita del día"</p>
         </div>
     </div>
 </div>
