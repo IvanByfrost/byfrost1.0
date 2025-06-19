29aad2992c37f7147c81eceb2ed445b138668afd
IvandProgram <ivandprogramador@gmail.com>
2025-06-19 15:29:42 -0500

    fix: restructure team member sections for better layout in about page

--- app/views/index/about.php
+++ app/views/index/about.php
@@ -12,24 +12,32 @@ require_once __DIR__ . '/../layouts/header.php';
     <div class="our-team">
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team1.webp" alt="Team Member 1">
+        </div>
+        <div class="desc-member">
             <h3>Iván Ruiz</h3>
             <p>CEO - Scrum Master</p>
             <p>"Cita del día"</p>
         </div>
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team2.webp" alt="Team Member 1">
+        </div>
+        <div class="desc-member">
             <h3>Johan Carrillo</h3>
             <p>Backend Developer</p>
             <p>"Cita del día"</p>
         </div>
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team3.webp" alt="Team Member 1">
+        </div>
+        <div class="desc-member">
             <h3>Lucía Nova</h3>
             <p>Backend Developer</p>
             <p>"Cita del día"</p>
         </div>
         <div class="photo">
             <img src="<?php echo url . rq ?>img\team4.webp" alt="Team Member 1">
+        </div>
+        <div class="desc-member">
             <h3>Laura Ariza</h3>
             <p>Frontend Developer</p>
             <p>"Cita del día"</p>
