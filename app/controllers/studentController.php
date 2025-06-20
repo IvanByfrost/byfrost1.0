6f377c7521717546e51210fad6711089971d7367
IvanByFrost <ivanwander99@gmail.com>
2025-06-19 20:53:25 -0500

    feat: implement student and subject controllers with constructor for database connection

--- app/controllers/studentController.php
+++ app/controllers/studentController.php
@@ -1,6 +1,11 @@
 <?php
 require_once ROOT . '/app/models/studentModel.php';
 
-
-
-?>
+class studentController extends MainController
+{
+    protected $dbConn;
+    public function __construct($dbConn)
+    {
+        $this->dbConn = $dbConn;
+    }
+}
--- app/controllers/subjectController.php
+++ app/controllers/subjectController.php
@@ -1,6 +1,13 @@
 <?php
 require_once ROOT . '/app/models/subjectModel.php';
 
+class subjectController extends MainController
+{
+    protected $dbConn;
+    public function __construct($dbConn)
+    {
+        $this->dbConn = $dbConn;
+    }
 
-
-?>
+    
+}
