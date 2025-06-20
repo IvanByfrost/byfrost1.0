2413657bff093f598192f887039a5389f118dab2
IvandProgram <ivandprogramador@gmail.com>
2025-06-20 14:41:42 -0500

    feat: reorganize sidebar menu structure for better clarity and accessibility

--- app/views/headMaster/hmasterSidebar.php
+++ app/views/headMaster/hmasterSidebar.php
@@ -3,14 +3,25 @@
         <ul>
             <li>
                 <a href="<?php ROOT; ?>#"><i data-lucide="home"></i>Inicio</a>
-            <ul class="submenu"></ul>
-            <ul class="submenu"></ul>
-            <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
             </li>
-            <li><a href="<?php ROOT; ?>#"><i data-lucide="school"></i>Colegios</a></li>
-            <li><a href="<?php ROOT; ?>#"><i data-lucide="user"></i>Coordinador</a></li>
-            <li><a href="<?php ROOT; ?>#"><i data-lucide="users"></i>Estudiantes</a></li>
-            <li><a href="<?php ROOT; ?>#"><i data-lucide="key"></i>Permisos</a></li>
+            <li><a href="<?php ROOT; ?>#"><i data-lucide="school"></i>Colegios</a>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+            </li>
+            <li><a href="<?php ROOT; ?>#"><i data-lucide="user"></i>Coordinador</a>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+            </li>
+            <li><a href="<?php ROOT; ?>#"><i data-lucide="users"></i>Estudiantes</a>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+                <ul class="submenu"></ul>
+            </li>
             <li><a href="<?php ROOT; ?>#"><i data-lucide="bar-chart-2"></i>Reportes</a></li>
             <li><a href="<?php ROOT; ?>#"><i data-lucide="settings"></i>Configuración</a></li>
         </ul>
