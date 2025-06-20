81a60779c28bcbaa602547bd7a93cb5200605123
IvanByFrost <ivanwander99@gmail.com>
2025-06-20 05:26:34 -0500

    feat: implement getAllSubjects method in subjectModel for retrieving all subjects

--- app/models/subjectModel.php
+++ app/models/subjectModel.php
@@ -8,6 +8,13 @@ class subjectModel extends mainModel
     }
 
     //Función para obtener todas las asignaturas
+    public function getAllSubjects()
+    {
+        $query = "SELECT * FROM subjects";
+        $stmt = $this->dbConn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
     // Función para crear una asignatura
     // Función para actualizar una asignatura
     // Función para eliminar una asignatura
