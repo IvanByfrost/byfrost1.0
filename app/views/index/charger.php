0160b9ad73a74b90208ef092b0090a5ee2550221
Iván Ruiz <ivanwander99@gmail.com>
2025-06-22 07:56:32 -0500

    fix: update loading page styles and improve logo display

--- app/views/index/charger.php
+++ app/views/index/charger.php
@@ -1,17 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+if (!defined('ROOT')) {
+    define('ROOT', dirname(__DIR__, 3));
+}
+require_once ROOT . '/config.php';
+?>
 
+<!DOCTYPE html>
+<html lang="es">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Byfrost - Cargando...</title>
   <link rel="stylesheet" href="<?php echo url . rq ?>css/loading.css">
+  <link rel="icon" type="image/x-icon" href="<?php echo url . rq ?>img/favicon.png">
 </head>
 
 <body>
   <div class="loading-cont">
-    <img class="BYFROST" src="Byfrost (6).svg" alt="cd" />
+    <img class="BYFROST" src="<?php echo url . rq ?>img/Byfrost-logo.svg" alt="loading-logo" />
     <h1>BYFROST</h1>
-    <canvas>¡Bienvenido de vuelta, <span id="userName"></span>!</canvas>
+    <p>¡Bienvenido de vuelta, <span id="userName">Iván</span>!</p>
   </div>
 </body>
 
