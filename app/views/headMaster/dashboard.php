a523e681c98486301d7cedadaf9f6f6b87012166
IvanByFrost <ivanwander99@gmail.com>
2025-06-25 19:37:27 -0500

    feat: mejorar la gestión de acceso en routerView.php, agregar verificación de sesión en dashboard.php y actualizar la barra lateral con enlaces dinámicos para la gestión de colegios y reportes

--- app/views/headMaster/dashboard.php
+++ app/views/headMaster/dashboard.php
@@ -1,24 +1,35 @@
 <?php
+session_start();
+if (!isset($_SESSION["ByFrost_id"])) {
+    header("Location: " . url . "views/index/login.php");
+    exit;
+}
 if (!defined('ROOT')) {
-    define('ROOT', dirname(dirname(dirname(__DIR__))));
+  define('ROOT', dirname(dirname(dirname(__DIR__))));
 }
 require_once ROOT . '/config.php';
 require_once ROOT . '/app/views/layouts/dashHeader.php';
 ?>
+<script>
+  const BASE_URL = "<?php echo url ?>";
+  console.log("Valor de BASE_URL: ", BASE_URL);
+</script>
+
+<script type="text/javascript" src="<?php echo url . app. rq ?>js/loadView.js"></script>
 
 <body>
-    <nav>
+<div class="dashboard-container">
+  <aside class="sidebar">
         <?php
-        require_once 'hmasterSidebar.php';
-        ?>
-    </nav>
+    require_once 'hmasterSidebar.php';
+    ?>
+  </aside>
+    <div id="mainContent">
 
-  
+  </div>
+</div>
 </body>
 
-<script src="https://unpkg.com/lucide@latest"></script>
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    lucide.createIcons();
-  });
-</script>
+<?php
+require_once __DIR__ . '/../layouts/dashFooter.php';
+?>
