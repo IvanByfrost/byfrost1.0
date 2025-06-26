5e4a97fea1dbf81aec1e1eb0b083fd9c35b799d9
gylisk <gylisk95@gmail.com>
2025-06-25 19:38:25 -0500

    feat: remove unused CSS files and add new styles for headMaster views, enhancing layout and user experience

--- app/views/headMaster/createHmaster.php
+++ app/views/headMaster/createHmaster.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Crear Nuevo Rector</title>
+    
+</head>
 <body>
     <h1>Crear Nuevo Rector</h1>
     <a href="xampp">Volver al Listado</a>
@@ -13,11 +21,12 @@
         <input type="email" id="email" name="email" required><br><br>
 
         <label for="password">Contraseña:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
+        <input type="password" id="password" name="password" required minlength="6" title="La contraseña debe tener al menos 6 caracteres"><br><br>
 
         <label for="telefono">Teléfono:</label><br>
-        <input type="text" id="telefono" name="telefono"><br><br>
+        <input type="tel" id="telefono" name="telefono" pattern="[0-9]{7,15}" title="Ingrese un número de teléfono válido (7 a 15 dígitos)"><br><br>
 
         <button type="submit">Guardar Rector</button>
     </form>
 </body>
+</html>
