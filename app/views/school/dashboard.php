90b5eeae2869fb090e385f2c156f278f6aa24d46
IvanByFrost <ivanwander99@gmail.com>
2025-06-25 19:05:46 -0500

    fix: corregir errores tipográficos en el archivo funciones.md y mejorar la claridad de la documentación

---
+++ app/views/school/dashboard.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+if (!isset($_SESSION["ByFrost_id"])) {
+    header("Location: " . url . "views/index/login.php");
+    exit;
+}
+if (!defined('ROOT')) {
+  define('ROOT', dirname(dirname(dirname(__DIR__))));
+}
+require_once ROOT . '/config.php';
+require_once ROOT . '/app/views/layouts/dashHeader.php';
+?>
+<script>
+  const BASE_URL = "<?php echo url ?>";
+  console.log("Valor de BASE_URL: ", BASE_URL);
+</script>
+
+<script type="text/javascript" src="<?php echo url . app. rq ?>js/loadView.js"></script>
+
+<body>
+<div class="dashboard-container">
+  <aside class="sidebar">
+        <?php
+    require_once 'rootSidebar.php';
+    ?>
+  </aside>
+    <div id="mainContent">
+
+  </div>
+</div>
+</body>
+
+<?php
+require_once __DIR__ . '/../layouts/dashFooter.php';
+?>
