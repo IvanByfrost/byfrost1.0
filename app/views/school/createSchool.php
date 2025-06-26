f820de1a57acc9452a9b852546427234c56b1837
gylisk <gylisk95@gmail.com>
2025-06-25 21:09:22 -0500

    feat: add location section to createSchool.php for detailed school address and contact information

--- app/views/school/createSchool.php
+++ app/views/school/createSchool.php
@@ -38,3 +38,30 @@
         </div>
       </li>
     </ul>
+
+    <h2>📍 Ubicación</h2>
+    <ul>
+      <li>
+        <label for="departamento">Departamento:</label>
+        <input type="text" id="departamento" name="departamento">
+      </li>
+      <li>
+        <label for="municipio">Municipio / Ciudad:</label>
+        <input type="text" id="municipio" name="municipio">
+      </li>
+      <li>
+        <label for="direccion">Dirección:</label>
+        <input type="text" id="direccion" name="direccion">
+      </li>
+      <li>
+        <label for="telefono">Teléfono de Contacto:</label>
+        <input type="tel" id="telefono" name="telefono">
+      </li>
+      <li>
+        <label for="correo">Correo Electrónico Institucional:</label>
+        <input type="email" id="correo" name="correo">
+      </li>
+    </ul>
+  </div>
+</body>
+</html>
