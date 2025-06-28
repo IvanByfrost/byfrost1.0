1d282944ffcdee1b84cef9ef2daf4a49b9f177f6
IvanByFrost <ivanwander99@gmail.com>
2025-06-27 21:46:28 -0500

    chore: remove obsolete test files for connection, main controller, session management, and unauthorized access

--- test-simple.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "¡Test simple funcionando!";
-echo "<br>";
-echo "Directorio actual: " . __DIR__;
-echo "<br>";
-echo "Nombre del directorio: " . basename(__DIR__);
-echo "<br>";
-echo "URL completa: " . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-?> 
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "¡Apache está funcionando correctamente!";
-echo "<br>";
-echo "Directorio actual: " . __DIR__;
-echo "<br>";
-echo "URL base: " . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-echo "<br>";
-echo "Puerto: " . ($_SERVER['SERVER_PORT'] ?? 'No especificado');
-?> 
